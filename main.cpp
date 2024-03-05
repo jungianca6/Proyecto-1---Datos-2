35daa1dfd2c7c3a3cf03b9bfcbe9c7db4c7d8ebd
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2024-03-05 02:48:24 -0600

    botones y caja de texto
    Faltaría ver como posicionarlos en la posicion deseada, y arreglar lo de la caja de texto

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <gtk/gtk.h>
 using namespace std;
 
+static void button_clicked(GtkWidget *widget, gpointer data) {
+    cout << "¡Botón clickeado!" << endl;
+}
+
 //Funcion que activa la ventana de la aplicacion
 static void activate(GtkApplication *app){
     // Crea una ventana
@@ -10,7 +14,37 @@ static void activate(GtkApplication *app){
     //Titulo de la ventana
     gtk_window_set_title(GTK_WINDOW(window),"Ventana");
     //Tamaño de la ventana
-    gtk_window_set_default_size(GTK_WINDOW(window), 500, 400);
+    gtk_window_set_default_size(GTK_WINDOW(window), 700, 500);
+    //gtk_window_set_resizable(GTK_WINDOW(window),false);
+
+    GtkWidget *button = gtk_button_new_with_label("Mi botón");
+    GtkWidget *button2 = gtk_button_new_with_label("Mi botón2");
+    GtkWidget *entry;
+
+    g_signal_connect(button, "clicked", G_CALLBACK(button_clicked),NULL);
+    g_signal_connect(button2, "clicked", G_CALLBACK(button_clicked),NULL);
+
+    entry = gtk_entry_new();
+    gtk_entry_set_max_length(GTK_ENTRY(entry),FALSE);
+
+
+    //algo como los paneles de java swing
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL,10);
+    GtkWidget *button_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 50);
+
+    // Añade el botón al contenedor
+    gtk_box_append(GTK_BOX(button_box), button);
+    gtk_box_append(GTK_BOX(button_box), button2);
+
+    gtk_widget_set_size_request(button, 150, 50);
+    gtk_widget_set_size_request(button2, 150, 50);
+
+    // Agrega el contenedor de botones al contenedor principal de la ventana
+    gtk_box_append(GTK_BOX(box), button_box);
+    gtk_box_append(GTK_BOX(box),entry);
+
+    // Añade el contenedor a la ventana
+    gtk_window_set_child(GTK_WINDOW(window), box);
 
     //Muestra la ventana
     gtk_widget_show(window);
