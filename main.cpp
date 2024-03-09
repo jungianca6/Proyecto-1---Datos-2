83edce910d3d21299560b443a56525e8560fe06a
SpaceBa13 <brayanalpizar36@gmail.com>
2024-03-08 18:32:49 -0600

    Implementacion del Hilo para el ServerSocket

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <string>
 #include "ServerSocket.h"
+#include "thread"
+#include "chrono"
+
+using namespace std;
+
+int hola(){
+    for(int i=0; i<100; i++){
+        sleep(1);
+        cout << "hola desde la otra funcion" + i << endl;
+    };
+    return 0;
+}
 
 int main() {
     int portNumber = 12346; // Puerto en el que escuchará el servidor
     ServerSocket servidor = ServerSocket(portNumber);
-    // Aceptar conexiones continuamente
-    servidor.acceptConnections();
+    thread hilo(&ServerSocket::acceptConnections, &servidor);
+    hola();
 
+    hilo.join();
     return 0;
 }
