47c059c8e0a76a4c828708d801ba770d1dea8eed
SpaceBa13 <brayanalpizar36@gmail.com>
2024-03-10 17:34:26 -0600

    Avance del archivo bin

--- main.cpp
+++ main.cpp
@@ -3,8 +3,18 @@
 #include "ServerSocket.h"
 #include "thread"
 #include "chrono"
+#include <stdio.h>
+#include <fstream>
 
 using namespace std;
+
+struct Cancion{
+    char nombre[30];
+    char artista[30];
+    int duracion;
+    int id;
+};
+
 
 int hola(){
     for(int i=0; i<100; i++){
@@ -18,6 +28,30 @@ int main() {
     int portNumber = 12346; // Puerto en el que escuchará el servidor
     ServerSocket servidor = ServerSocket(portNumber);
     thread hilo(&ServerSocket::acceptConnections, &servidor);
+
+    // Nombre del archivo binario en el que deseas escribir
+    string filename = "/home/spaceba/CLionProjects/Server/archivo.bin";
+
+    // Abrir el archivo en modo binario para escritura
+    ofstream archivo(filename, ios::binary | ios::app);
+
+    // Verificar si el archivo se abrió correctamente
+    if (!archivo.is_open()) {
+        std::cerr << "Error al abrir el archivo " << filename << std::endl;
+        return 1;
+    }
+
+    // Datos que deseas escribir en el archivo
+    Cancion cancion = {"Creo-Rock-Thing", "Brayan", 10,10};
+
+    // Escribir los datos en el archivo binario
+    archivo.write(reinterpret_cast<const char *>(&cancion), sizeof(cancion));
+
+    // Cerrar el archivo al finalizar
+    archivo.close();
+
+
+
 
     hilo.join();
     return 0;
