aa316999b148c45cbcca97f325c9ed3ccd5d956f
SpaceBa13 <brayanalpizar36@gmail.com>
2024-03-25 17:58:28 -0600

    Formato de respuesta para el envio de la lista

--- ServerSocket.cpp
+++ ServerSocket.cpp
@@ -48,6 +48,7 @@ void ServerSocket::acceptConnections() {
             "siguiente":{"data":{"nombre":"B","artista":"Brayan","duracion":320,"id":2},"siguiente"
                 :{"data":{"nombre":"C","artista":"Brayan","duracion":320,"id":3},"siguiente":null}}}})";
 
+
             //Envia la respuesta al cliente
             send_response(command, "OK", clientSocket, lista);
             close(clientSocket);
