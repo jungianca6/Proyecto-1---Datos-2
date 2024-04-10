055f0fe87af64bb2acfee68246a76f982a73f030
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2024-04-09 22:46:52 -0600

    que bendicion

---
+++ cmake-build-debug/CMakeFiles/Proyecto_1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dell/Escritorio/Proyecto-1-Datos-2/Admin_paginas.cpp" "CMakeFiles/Proyecto_1.dir/Admin_paginas.cpp.o" "gcc" "CMakeFiles/Proyecto_1.dir/Admin_paginas.cpp.o.d"
+  "/home/dell/Escritorio/Proyecto-1-Datos-2/DoubleList.cpp" "CMakeFiles/Proyecto_1.dir/DoubleList.cpp.o" "gcc" "CMakeFiles/Proyecto_1.dir/DoubleList.cpp.o.d"
+  "/home/dell/Escritorio/Proyecto-1-Datos-2/PagedArray.cpp" "CMakeFiles/Proyecto_1.dir/PagedArray.cpp.o" "gcc" "CMakeFiles/Proyecto_1.dir/PagedArray.cpp.o.d"
+  "/home/dell/Escritorio/Proyecto-1-Datos-2/Paged_Array.cpp" "CMakeFiles/Proyecto_1.dir/Paged_Array.cpp.o" "gcc" "CMakeFiles/Proyecto_1.dir/Paged_Array.cpp.o.d"
+  "/home/dell/Escritorio/Proyecto-1-Datos-2/Pagina.cpp" "CMakeFiles/Proyecto_1.dir/Pagina.cpp.o" "gcc" "CMakeFiles/Proyecto_1.dir/Pagina.cpp.o.d"
+  "/home/dell/Escritorio/Proyecto-1-Datos-2/ServerSocket.cpp" "CMakeFiles/Proyecto_1.dir/ServerSocket.cpp.o" "gcc" "CMakeFiles/Proyecto_1.dir/ServerSocket.cpp.o.d"
+  "/home/dell/Escritorio/Proyecto-1-Datos-2/main.cpp" "CMakeFiles/Proyecto_1.dir/main.cpp.o" "gcc" "CMakeFiles/Proyecto_1.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
