59d93110f897dde143cf893ffe5170350c7e1b93
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2024-04-09 21:52:27 -0600

    cmake

---
+++ cmake-build-debug/CMakeFiles/Proyecto_1.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_1.dir/Admin_paginas.cpp.o"
+  "CMakeFiles/Proyecto_1.dir/Admin_paginas.cpp.o.d"
+  "CMakeFiles/Proyecto_1.dir/DoubleList.cpp.o"
+  "CMakeFiles/Proyecto_1.dir/DoubleList.cpp.o.d"
+  "CMakeFiles/Proyecto_1.dir/PagedArray.cpp.o"
+  "CMakeFiles/Proyecto_1.dir/PagedArray.cpp.o.d"
+  "CMakeFiles/Proyecto_1.dir/Paged_Array.cpp.o"
+  "CMakeFiles/Proyecto_1.dir/Paged_Array.cpp.o.d"
+  "CMakeFiles/Proyecto_1.dir/Pagina.cpp.o"
+  "CMakeFiles/Proyecto_1.dir/Pagina.cpp.o.d"
+  "CMakeFiles/Proyecto_1.dir/ServerSocket.cpp.o"
+  "CMakeFiles/Proyecto_1.dir/ServerSocket.cpp.o.d"
+  "CMakeFiles/Proyecto_1.dir/main.cpp.o"
+  "CMakeFiles/Proyecto_1.dir/main.cpp.o.d"
+  "Proyecto_1"
+  "Proyecto_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
